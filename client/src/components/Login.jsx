93dc12c56a8b8daacfd5cd0b85cb2f5467642790
Eray Sahin <eraysahinmoon@gmail.com>
2023-10-21 19:03:05 -0400

    Added Login Form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
     <div>
       <Navbar />
-      <h1>Login</h1>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-8 rounded shadow-lg w-80">
+          <h2 className="text-2xl font-semibold mb-4">Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-600 text-sm font-medium mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full p-2 border rounded"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block text-gray-600 text-sm font-medium mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full p-2 border rounded"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <button
+                className='bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded w-full"
+            '
+              >
+                Create Account
+              </button>
+              <button
+                type="submit"
+                className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded w-full"
+              >
+                Log In
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
